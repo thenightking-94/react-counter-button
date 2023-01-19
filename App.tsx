14cd894d59c33baa69a1909a806ba9f7d04dc31e
thenightking-94 <67338546+thenightking-94@users.noreply.github.com>
2023-01-19 12:33:04 +0530

    React timer added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,47 @@
 import * as React from 'react';
 import './style.css';
 
-export default function App() {
+const flexBox: Record<string, unknown> = {
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  width: '300px',
+  marginTop: '50px',
+} as Record<string, unknown>;
+
+export default function App(): React.ReactElement {
+  const [counter, setcounter] = React.useState<number>(0);
+  const pausedValue = React.useRef(null);
+  let Timerfn = React.useRef(undefined);
+
+  React.useEffect(() => {
+    if (counter > 1) pausedValue.current = counter;
+  }, [counter]);
+
+  const startTimer = () => {
+    Timerfn.current = setInterval(() => {
+      if (counter === 0) setcounter((counter) => counter + 1);
+      else {
+        setcounter(pausedValue.current + 1);
+      }
+    }, 1000);
+  };
+
+  const pauseTimer = () => {
+    clearInterval(Timerfn.current);
+  };
+
+  const resetTimer = () => {
+    setcounter(0);
+    clearInterval(Timerfn.current);
+  };
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div style={flexBox}>
+      {counter}
+      <button onClick={startTimer}>Start</button>
+      <button onClick={pauseTimer}>Pause</button>
+      <button onClick={resetTimer}>Reset</button>
     </div>
   );
 }
